12c8ca64956d5794fdc7bdf1d23f3edc77c5d1ed
Syed Qasim Rehman <64010656+Syedqasimrehman@users.noreply.github.com>
2024-08-29 20:57:41 +0500

    Update Highlights.jsx

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -11,13 +11,13 @@ useGSAP(() =>{
 } ,[])
   return (
     <section
-      id="highelights"
+      id="highlights"
       className="w-screen overflow-hidden h-full common-padding bg-zinc"
     >
       <div className="screen-max-width">
         <div className='mb-12 w-full md:flex items-end justify-between'>
           <h1 id="title" className="section-heading">
-            Get the highelights.
+            Get the highlights.
           </h1>
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
